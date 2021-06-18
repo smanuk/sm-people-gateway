d32ecdd3be4d2e70e3ce341643315f6c7883c1f1
Steven Manikiam <sman@manikiam.net>
2021-06-18 15:32:26 +0100

    basic project files.

---
+++ src/main/java/net/manikiam/gw/smpeoplegateway/SmPeopleGatewayApplication.java
@@ -0,0 +1,13 @@
+package net.manikiam.gw.smpeoplegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmPeopleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmPeopleGatewayApplication.class, args);
+	}
+
+}
